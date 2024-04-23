c69a39fa6597d7d9f56ea997a8896933d4736695
devfabien <ish001fabi@gmail.com>
2024-04-23 12:20:30 +0200

    feat: add the test to check if the heading is available

---
+++ test/components/TermsAndConditions.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+
+describe("Terms and Conditions", () => {
+  it("should render with correct text and initial state", () => {
+    render(<TermsAndConditions />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Terms & Conditions");
+  });
+});
