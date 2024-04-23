e6b7d79719a623e403ee899a8bcd264dfcd5f814
devfabien <ish001fabi@gmail.com>
2024-04-23 09:48:20 +0200

    feat: create the test to render a button when name is not provided

--- src/test/components/Greet.test.tsx
+++ src/test/components/Greet.test.tsx
@@ -10,4 +10,10 @@ describe("Greet", () => {
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent(/fabien/i);
   });
+  it("should render a login button when name is not provided", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
 });
