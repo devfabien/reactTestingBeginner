4c1fac6c9a83bb9ee253e1e30c9aa727f7ad68c2
devfabien <ish001fabi@gmail.com>
2024-04-23 12:50:27 +0200

    feat: add the terms and condition test

--- test/components/TermsAndConditions.test.tsx
+++ test/components/TermsAndConditions.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
 
 describe("Terms and Conditions", () => {
   it("should render with correct text and initial state", () => {
@@ -16,5 +17,15 @@ describe("Terms and Conditions", () => {
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
+  });
+
+  it("should should enable the button when the checkbox is checked", async () => {
+    render(<TermsAndConditions />);
+
+    const checkbox = screen.getByRole("checkbox");
+    const user = userEvent.setup();
+    await user.click(checkbox);
+
+    expect(screen.getByRole("button")).toBeEnabled();
   });
 });
