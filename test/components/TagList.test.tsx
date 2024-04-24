ebc1b9349d135e670a035f8b953f4ad6d9937b2b
devfabien <ish001fabi@gmail.com>
2024-04-24 16:20:03 +0200

    refactor: add await for the waitFor statement

--- test/components/TagList.test.tsx
+++ test/components/TagList.test.tsx
@@ -2,10 +2,10 @@ import { render, screen, waitFor } from "@testing-library/react";
 import TagList from "../../src/components/TagList";
 
 describe("Taglist", () => {
-  it("should render the component", () => {
+  it("should render the component", async () => {
     render(<TagList />);
 
-    waitFor(() => {
+    await waitFor(() => {
       const listItems = screen.getAllByRole("listitem");
       expect(listItems.length).toBeGreaterThan(0);
     });
