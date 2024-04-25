bc9343c2477000419140dc698702d5d25e876645
devfabien <ish001fabi@gmail.com>
2024-04-25 12:05:21 +0200

    feat: create a test for the toaster model

---
+++ test/components/ToastDemo.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import ToastDemo from "../../src/components/ToastDemo";
+import { Toaster } from "react-hot-toast";
+import userEvent from "@testing-library/user-event";
+
+describe("Toast demo", () => {
+  it("should render the Toast", async () => {
+    render(
+      <>
+        <ToastDemo />
+        <Toaster />
+      </>
+    );
+
+    const button = screen.getByRole("button");
+    const user = userEvent.setup();
+    await user.click(button);
+
+    const toast = await screen.findByText(/success/i);
+    expect(toast).toBeInTheDocument();
+  });
+});
