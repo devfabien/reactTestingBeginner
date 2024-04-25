8dc4135296ed1f0bac1beebe8a1e4f298756ee3e
devfabien <ish001fabi@gmail.com>
2024-04-25 12:09:31 +0200

    fix: remove the deprecated functions

--- test/setup.ts
+++ test/setup.ts
@@ -6,8 +6,6 @@ Object.defineProperty(window, "matchMedia", {
     matches: false,
     media: query,
     onchange: null,
-    addListener: vi.fn(), // Deprecated
-    removeListener: vi.fn(), // Deprecated
     addEventListener: vi.fn(),
     removeEventListener: vi.fn(),
     dispatchEvent: vi.fn(),
