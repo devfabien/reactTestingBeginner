ae3d97a3a3f0a64601012bedd70ecb0aeb9e4189
devfabien <ish001fabi@gmail.com>
2024-04-29 09:24:39 +0200

    feat: configure resizeObserver to be global

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,7 @@
 import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+
+global.ResizeObserver = ResizeObserver;
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
