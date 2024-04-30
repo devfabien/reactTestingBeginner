45c92d306e5c5c8a32aa5d542b6c224edffa4fd1
devfabien <ish001fabi@gmail.com>
2024-04-30 12:28:47 +0200

    feat: add a test to check if the backend mock is correctly setup

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,4 +4,10 @@ describe("Main", () => {
   it("should check for truthy value", () => {
     expect(1).toBeTruthy();
   });
+  it("should check if the backend is collectly setup", async () => {
+    const response = await fetch("/categories");
+    const data = await response.json();
+    console.log(data);
+    expect(data).toHaveLength(3);
+  });
 });
