6f315972ccad266bc64a0e32a64da6b1329804b6
devfabien <ish001fabi@gmail.com>
2024-05-01 10:17:36 +0200

    feat: add the handler for getting products

--- test/mocks/handlers.ts
+++ test/mocks/handlers.ts
@@ -8,4 +8,11 @@ export const handlers = [
       { id: 3, name: "Accesories" },
     ]);
   }),
+  http.get("/products", () => {
+    return HttpResponse.json([
+      { id: 1, name: "Product 1" },
+      { id: 2, name: "Product 2" },
+      { id: 3, name: "Product 3" },
+    ]);
+  }),
 ];
