77ef238a291542652c555ac91b6b1678ef6fb2c8
devfabien <ish001fabi@gmail.com>
2024-05-02 10:49:40 +0200

    feat: create a display a message if no product is found

--- test/components/ProductDetails.test.tsx
+++ test/components/ProductDetails.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import ProductDetail from "../../src/components/ProductDetail";
 import { products } from "../mocks/data";
+import { server } from "../mocks/server";
+import { http, HttpResponse } from "msw";
 
 describe("Product Details", () => {
   it("should render a list of products", async () => {
@@ -11,5 +13,12 @@ describe("Product Details", () => {
     expect(
       await screen.findByText(new RegExp(products[0].price.toString()))
     ).toBeInTheDocument();
+  });
+  it("should render a message if no product is found", async () => {
+    server.use(http.get("/products/1", () => HttpResponse.json(null)));
+
+    render(<ProductDetail productId={1} />);
+    const message = await screen.findByText(/not found/);
+    expect(message).toBeInTheDocument();
   });
 });
