af26605adb821d39b2a791f01129a68b0daaf49b
devfabien <ish001fabi@gmail.com>
2024-05-03 09:48:16 +0200

    feat: create the database mock using fakerjs and mswjs/data

---
+++ test/mocks/db.ts
@@ -0,0 +1,10 @@
+import { factory, primaryKey } from "@mswjs/data";
+import { faker } from "@faker-js/faker";
+
+export const db = factory({
+  product: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.productName,
+    price: () => faker.number.int({ min: 1, max: 100 }),
+  },
+});
